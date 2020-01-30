01bea7e058680f83928cd1a9b1f33c0d38c30d23
Alumnos <Alumnos@LDC-PC2>
2020-01-30 13:38:34 -0600

    definiendo la clase usuario en su totalidad

--- src/mx/uam/ayd/sgp/modelo/Usuario.java
+++ src/mx/uam/ayd/sgp/modelo/Usuario.java
@@ -10,15 +10,21 @@ public class Usuario {
 	private int idEmpleado;
 	
 	
-	public Usuario(String usuario, String contraseña, String tipoCuenta){
+	public Usuario(String usuario, String contraseña, String tipoCuenta, String nombre, String apellidos, String telefono, int idEmpleado){
 		this.contraseña = contraseña;
 		this.usuario = usuario;
 		this.tipoCuenta = tipoCuenta;
-		
+		this.nombre=nombre;
+		this.apellidos=apellidos;
+		this.telefono=telefono;
+		this.idEmpleado=idEmpleado;
 	}
 	
 	public String getUsuario(){
 		return usuario;
+	}
+	public String getNombre() {
+		return this.nombre + this.apellidos;
 	}
 	public String getContraseña(){
 		return contraseña;
