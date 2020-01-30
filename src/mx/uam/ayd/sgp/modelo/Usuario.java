2ac74edb10a65aad5627d74f70c29e94ba0993be
Alumnos <Alumnos@LDC-PC2>
2020-01-30 13:25:25 -0600

    en proceso terminacion de la clase usuario

--- src/mx/uam/ayd/sgp/modelo/Usuario.java
+++ src/mx/uam/ayd/sgp/modelo/Usuario.java
@@ -4,6 +4,11 @@ public class Usuario {
 	private String usuario;
 	private String contraseña;
 	private String tipoCuenta;
+	private String nombre;
+	private String apellidos;
+	private String telefono;
+	private int idEmpleado;
+	
 	
 	public Usuario(String usuario, String contraseña, String tipoCuenta){
 		this.contraseña = contraseña;
