b410c632b8afb8a9185df3f0b308bc877ac72a5d
hernandev96 <55611371+hernandev96@users.noreply.github.com>
2020-02-06 12:05:43 -0600

    Add files via upload

--- src/mx/uam/ayd/sgp/negocio/ControlAdminUsuario.java
+++ src/mx/uam/ayd/sgp/negocio/ControlAdminUsuario.java
@@ -17,14 +17,14 @@ public class ControlAdminUsuario {
 	  * Metodo que inicia la ventana de registro del usuario
 	  * **/
 	public void inicia() {
-		adduser=new VentanaAddUser(this);
+		adduser=new VentanaAddUser();
 		adduser.setVisible(true);
 		adduser.setLocationRelativeTo(null);
 	}
 	
 	
 	public boolean adduser(String nombre,String apellidos,String telefono,int idempleado,String cargo,String user,String pass) {
-		
+		daouser=new DAOUsuario();
 		Usuario useradd=new Usuario(user,pass,cargo,nombre,apellidos,telefono,idempleado);	
 		
 		if(daouser.agregaUsuario(useradd)==true) {
