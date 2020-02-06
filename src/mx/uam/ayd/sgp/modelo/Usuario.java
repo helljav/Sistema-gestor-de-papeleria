e08067d52ffeb36c6b03d09ad1739158b8ac13ef
hernandev96 <55611371+hernandev96@users.noreply.github.com>
2020-02-06 12:05:22 -0600

    Add files via upload

--- src/mx/uam/ayd/sgp/modelo/Usuario.java
+++ src/mx/uam/ayd/sgp/modelo/Usuario.java
@@ -2,7 +2,7 @@ package mx.uam.ayd.sgp.modelo;
 
 public class Usuario {
 	private String usuario;
-	private String contraseÃ±a;
+	private String contraseña;
 	private String tipoCuenta;
 	private String nombre;
 	private String apellidos;
@@ -10,8 +10,8 @@ public class Usuario {
 	private int idEmpleado;
 	
 	
-	public Usuario(String usuario, String contraseÃ±a, String tipoCuenta, String nombre, String apellidos, String telefono, int idEmpleado){
-		this.contraseÃ±a = contraseÃ±a;
+	public Usuario(String usuario, String contraseña, String tipoCuenta, String nombre, String apellidos, String telefono, int idEmpleado){
+		this.contraseña = contraseña;
 		this.usuario = usuario;
 		this.tipoCuenta = tipoCuenta;
 		this.nombre=nombre;
@@ -23,8 +23,8 @@ public class Usuario {
 	public String getUsuario(){
 		return usuario;
 	}
-	public String getContraseÃ±a(){
-		return contraseÃ±a;
+	public String getContraseña(){
+		return contraseña;
 	}
 	public String getTipoCuenta() {
 		return tipoCuenta;
