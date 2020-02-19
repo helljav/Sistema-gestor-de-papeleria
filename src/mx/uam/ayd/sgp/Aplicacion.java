74b0485fc4bfcca41334f107db5e6ee379aabb3f
Eduardo oseguera S <lalo_infrax@hotmail.com>
2020-02-19 17:19:02 -0600

    prueba
    eduardo

--- src/mx/uam/ayd/sgp/Aplicacion.java
+++ src/mx/uam/ayd/sgp/Aplicacion.java
@@ -47,7 +47,7 @@ public class Aplicacion {
 	public Aplicacion() {
 		daousuario = new DAOUsuario();
 	}
-
+//hola que tsl
 	/**
 	 * Este metodo inicia la aplicacion e inicia primero la autentificacion del
 	 * usuario llamando a la ventana IniciarSesion por medio del
